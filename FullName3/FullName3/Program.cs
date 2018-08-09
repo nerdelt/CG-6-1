73de151c5936f56a5debf48b863d8a34455acad3
nerdelt <nerdelt@gmail.com>
2018-08-09 15:17:09 -0500

    Another Attempt with StringBuilder in Method

--- FullName3/FullName3/Program.cs
+++ FullName3/FullName3/Program.cs
@@ -16,11 +16,16 @@ namespace FullName3
             Console.Write("Please enter your first name: ");
             string firstName = Console.ReadLine();
 
-
             //ask user for last name
             Console.Write("Please enter your last name: ");
             string lastName = Console.ReadLine();
 
+           /*
+            * Initally this is how I did the assignment, but I was so scared I would get points 
+            * off for not including it in a method and returning a new value that I went ahead and 
+            * put it inside a method. Just wanted to know if this was a correct way of 
+            * tacking the assignment as well, so I left it in.
+            
             //create StringBuilder to format name string
              StringBuilder fullName = new StringBuilder();
 
@@ -30,18 +35,9 @@ namespace FullName3
             //prints result, using FullName method
             Console.WriteLine($"Hello, {fullName}!");
             Console.ReadLine();
-
-
-            /*ok, I'm not sure if i'm complete the assignment correctly because...
-            do I need to put the string builder inside a method? 
-            I did this just to make sure that I'm creating a method that combines the name 
-            and returns a new value (so I don't get points off) .
-            I've tested this and it works; the only thing I would need to do (besides uncommenting the method)
-            is remove an extra curly brace below the method.
-
-            This is basically what the program would look like without line 25,28,31, & 32.
             
-            
+            */
+
             string name = FullName(firstName, lastName);
             Console.WriteLine($"Hello, {name}!");
             Console.ReadLine();
@@ -49,19 +45,20 @@ namespace FullName3
         }
             private static string FullName(string firstName, string lastName)
             {
+                //creates a string builder to combine strings 
                  StringBuilder fullName = new StringBuilder();
+
+                //combines strings and formats full name
                  fullName.AppendFormat("{0} {1}", firstName, lastName);
 
                  //returns new StringBuilder value as string  
                  return fullName.ToString();
              }
          
-               
-            */
         }
 
     }
-}
+
 
 
     
